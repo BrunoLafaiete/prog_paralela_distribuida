c17347a294c1b5df95a70447d9a82509d4f0f8ad
bruna_nb <brunaneto.b@gmail.com>
2023-10-29 16:40:54 -0300

    ajustes nos algoritmos

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include 'constants.h'
+#include <time.h>
+#include <string.h>
 
 struct timeval t1, t2;
 
@@ -10,6 +11,21 @@ void trocar(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
+}
+
+int* gerarArrayAleatorio(int tamanho) {
+    srand(time(NULL));
+    int* array = (int*)malloc(sizeof(int) * tamanho);
+    if (array == NULL) {
+        printf("Falha na alocação de memória.\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < tamanho; i++) {
+        array[i] = rand();
+    }
+
+    return array;
 }
 
 // Função de ordenação Bubble Sort
@@ -24,40 +40,52 @@ void bubbleSort(int vetor[], int tamanho) {
 }
 
 int main() {
-    const int* arrays = {constants.ARRAY_SIZE_10, constants.ARRAY_SIZE_100, constants.ARRAY_SIZE_500, constants.ARRAY_SIZE_1000, constants.ARRAY_SIZE_5000, constants.ARRAY_SIZE_7000, constants.ARRAY_SIZE_10000, constants.ARRAY_SIZE_20000, constants.ARRAY_SIZE_50000, constants.ARRAY_SIZE_1000000};
-    int n_repeticoes = 1;
+    int n_repeticoes = 50;
+    int tamanhos[9] = {10, 100, 1000, 5000, 10000, 20000, 50000, 100000, 500000};
 
-    // printf("Vetor original: ");
-    // for (int i = 0; i < tamanho; i++) {
-    //    printf("%d ", vetor[i]);
-    // }
-    // printf("\n");
-
-    for (int i = 0; i < sizeof(arrays); i++) {
+    for (int i = 0; i < 9; i++) {
+        int tamanho_do_array = tamanhos[i];
         double tempo_total = 0;
 
-        for (int j = 0; j < n_repeticoes; i++) {
-            gettimeofday(&t1, NULL);	
-            bubbleSort(vetor, tamanho); // Chama a função de ordenação Bubble Sort
+        int* array_to_order = gerarArrayAleatorio(tamanho_do_array);
+
+        for (int j = 0; j < n_repeticoes; j++) {
+            // if (j==0 && (tamanho_do_array == 10 || tamanho_do_array == 100)) {
+            //     printf("array para ordenar \n");
+            //     for (int s = 0; s < tamanho_do_array; s++) {
+            //         printf("%d ", array_to_order[s]);
+            //     }
+            //     printf("\n");
+            // }
+
+            int* array_copy = malloc(sizeof(int) * tamanho_do_array); // Cria uma cópia do vetor
+            memcpy(array_copy, array_to_order, sizeof(int) * tamanho_do_array);
+
+            gettimeofday(&t1, NULL);
+            bubbleSort(array_copy, tamanho_do_array); // Chama a função de ordenação Bubble Sort
             gettimeofday(&t2, NULL);
             double t_ordenacao = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec) / 1000000.0);
             tempo_total += t_ordenacao;
+
+            // if (j == 0 && (tamanho_do_array == 10 || tamanho_do_array == 100)) {
+            //     printf("array ordenado \n");
+            //     for (int s = 0; s < tamanho_do_array; s++) {
+            //         printf("%d ", array_copy[s]);
+            //     }
+            //     printf("\n");
+            // }
+
+            free(array_copy);
         }
 
         double tempo_medio = tempo_total / n_repeticoes;
-        printf("Array de tamanho %f ordenado.", sizeof(arrays[i]));
-        printf("%f iterações realizadas. \n", n_repeticoes);
-        printf("Tempo médio de ordenação: %f", tempo_medio);
+        printf("\n");
+        printf("Array de tamanho %u ordenado. \n", tamanho_do_array);
+        printf("%d iterações realizadas. \n", n_repeticoes);
+        printf("Tempo médio de ordenação: %f.\n", tempo_medio);
+        printf("\n");
     }
-
-    // printf("Vetor ordenado: ");
-    // for (int i = 0; i < tamanho; i++) {
-    //     printf("%d ", vetor[i]);
-    //}
-    // printf("\n");
     
-    // printf("tempo total = %f\n", t_total);
-
     return 0;
 }
 
--- oddEven_openmp.c
+++ oddEven_openmp.c
@@ -1,69 +1,122 @@
 #include <stdio.h>
 #include <omp.h>
 #include <sys/time.h>
-
-#define n 8
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct timeval t1, t2;
 
-int a[] = {2, 1, 4, 9, 5, 3, 6, 10};
-
-void compare(int index)
+void compare(int vetor[], int index1, int index2)
 {
-    if (a[index] > a[index + 1] && index + 1 < n)
+    if (vetor[index1] > vetor[index2])
     {
-        int temp = a[index];
-        a[index] = a[index + 1];
-        a[index + 1] = temp;
+        int temp = vetor[index1];
+        vetor[index1] = vetor[index2];
+        vetor[index2] = temp;
     }
 }
 
-void oddEven()
+void oddEven(int vetor[], int tamanho)
 {
     int i, j;
-    for (i = 1; i <= n; i++)
+    for (i = 0; i < tamanho; i++)
     {
-        if (i % 2 == 1)
+        if (i % 2 == 0)
         {
-#pragma omp parallel for shared(a) private(j) 
-            for (j = 0; j < n - 1; j += 2)
+            #pragma omp parallel for
+            for (j = 1; j < tamanho - 1; j += 2)
             {
-                compare(j);
+                compare(vetor, j, j + 1);
             }
         }
         else
         {
-#pragma omp parallel for shared(a) private(j) 
-            for (j = 1; j < n - 1; j += 2)
+            #pragma omp parallel for
+            for (j = 0; j < tamanho - 1; j += 2)
             {
-                compare(j);
+                compare(vetor, j, j + 1);
             }
         }
     }
 }
 
-void printArray()
-{
-    int i;
-    for (i = 0; i < n; i++)
-        printf("%d ", a[i]);
-    printf("\n");
+int* gerarArrayAleatorio(int tamanho) {
+    srand(time(NULL));
+    int* array = (int*)malloc(sizeof(int) * tamanho);
+    if (array == NULL) {
+        printf("Falha na alocação de memória.\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < tamanho; i++) {
+        array[i] = rand()%1000;
+    }
+
+    return array;
 }
 
-int main()
-{
-    printf("Given array is: ");
-    printArray();
+// void printArray()
+// {
+//     int i;
+//     for (i = 0; i < n; i++)
+//         printf("%d ", a[i]);
+//     printf("\n");
+// }
 
-    gettimeofday(&t1, NULL);
-    oddEven();
-    gettimeofday(&t2, NULL);
+int main() {
+    int n_repeticoes = 5;
+    int tamanhos[1] = {500000};
+    int n_threads[2] = {16, 32};
 
-    printf("\nSorted array is: ");
-    printArray();
-    
-    double t_total = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec) / 1000000.0);
-    printf("tempo total = %f\n", t_total);
+    for (int i = 0; i < 1; i++) {
+        int tamanho_do_array = tamanhos[i];
+
+        int* array_to_order = gerarArrayAleatorio(tamanho_do_array);
+
+        for (int t = 0; t < 5; t++) {
+            int max_threads = n_threads[t];
+            omp_set_num_threads(max_threads);
+            double tempo_total = 0;
+
+            for (int j = 0; j < n_repeticoes; j++) {
+                // if (j==0 && (tamanho_do_array == 10 || tamanho_do_array == 100)) {
+                //     printf("array para ordenar \n");
+                //     for (int s = 0; s < tamanho_do_array; s++) {
+                //         printf("%d ", array_to_order[s]);
+                //     }
+                //     printf("\n");
+                // }
+
+                int* array_copy = malloc(sizeof(int) * tamanho_do_array); // Cria uma cópia do vetor
+                memcpy(array_copy, array_to_order, sizeof(int) * tamanho_do_array);
+                gettimeofday(&t1, NULL);
+                oddEven(array_copy, tamanho_do_array);
+                gettimeofday(&t2, NULL);
+                double t_ordenacao = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec) / 1000000.0);
+                tempo_total += t_ordenacao;
+
+                // if (j == 0 && (tamanho_do_array == 10 || tamanho_do_array == 100)) {
+                //     printf("array ordenado \n");
+                //     for (int s = 0; s < tamanho_do_array; s++) {
+                //         printf("%d ", array_copy[s]);
+                //     }
+                //     printf("\n");
+                // }
+                free(array_copy);
+            }
+
+            double tempo_medio = tempo_total / n_repeticoes;
+            printf("\n");
+            printf("Numero de Threads utilizadas: %u \n", max_threads);
+            printf("Array de tamanho %u ordenado. \n", tamanho_do_array);
+            printf("%d iterações realizadas. \n", n_repeticoes);
+            printf("Tempo médio de ordenação: %f.\n", tempo_medio);
+            printf("\n");
+        }
+
+        free(array_to_order);
+    }
 
     return 0;
 }
